c28219aa8d693d121b0ca3c2d4ad425e9bbe0870
Ömer Davarcı <omerdavarci.user@gmail.com>
2022-11-28 15:45:17 +0300

    Read and Write Data

---
+++ Database/demo.js
@@ -0,0 +1,3 @@
+var tc = "12312312322", name = "Osman", lname = "Abul", insurance = "SSK", sex = "M", age = 55;
+var query = `INSERT INTO HASTA VALUES('${tc}', '${name}', '${lname}', '${insurance}', '${sex}', ${age};`;
+console.log(query);
