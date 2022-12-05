f8450707e0d1ec297e7b6f266cb984f200d33119
Ömer Davarcı <omerdavarci.user@gmail.com>
2022-12-05 10:37:57 +0300

    Database Queries added
    index.js updated. Now it connects us to Database .
    read_data.js updated. Queries writed.

--- Database/read_data.js
+++ Database/read_data.js
@@ -1,12 +1,117 @@
 const { getClient } = require('./get-client');
 
-(async () => {
+async function getPatientsName()
+{
     const client = await getClient();
+    const entries = await client.query(`SELECT Ad, Soyad FROM HASTA;`);
 
-    var name = 'Osman';
-    const entries = await client.query(`SELECT * FROM HASTA WHERE ad = '${name}';`);
-    console.log(`Database entries for ${name}: ${entries.rowCount} row(s)`);
-    console.log(Object.keys(entries.rows?.[0]).join('\t'));
-    console.log(`${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`);
+    str = `${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`
+    out = str.split("\n")
+    for (let i = 0; i < out.length; i++)
+    {
+        out[i] = out[i].split("\t")
+    }
+
     await client.end();
-})();
+    return out;
+}
+
+async function getDepartmentName() {
+    const client = await getClient();
+    const entries = await client.query(`SELECT p_isim FROM POLIKLINIK;`);
+
+    str = `${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`
+    out = str.split("\n")
+    for (let i = 0; i < out.length; i++) {
+        out[i] = out[i].split("\t")
+    }
+
+    await client.end();
+    return out;
+}
+
+async function getDoctorsByDepartment() {
+    const client = await getClient();
+    const entries = await client.query(`SELECT p_no, p_isim FROM POLIKLINIK;`);
+
+    var toReturn = {};
+
+    var str = `${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`
+    departments = str.split("\n")
+    for (let i = 0; i < departments.length; i++) {
+        departments[i] = departments[i].split("\t")
+    }
+    
+    for (let i = 0; i < departments.length; i++) {
+        p_no = departments[i][0];
+        var doktorsEntry = await client.query(`Select ad || ' ' || soyad From Doktor where p_no = '${p_no}'`);
+        var doktorsStr = `${doktorsEntry.rows.map((r) => Object.values(r).join('\t')).join('\n')}`;
+        var doktorArr = doktorsStr.split("\n");
+        toReturn[departments[i][1]] = doktorArr;
+    }
+    
+    await client.end();
+    return toReturn;
+}
+
+async function getAppointmentsOfPatient(tc) {
+    const client = await getClient();
+    const entries = await client.query(`SELECT * FROM Randevu WHERE hasta_tc = '${tc}';`);
+
+    str = `${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`;
+    out = str.split("\n");
+    for (let i = 0; i < out.length; i++) {
+        out[i] = out[i].split("\t");
+    }
+
+    await client.end();
+    return out;
+}
+
+async function getSickReportsOfPatient(tc) {
+    const client = await getClient();
+    const entries = await client.query(`SELECT b_no FROM Randevu WHERE hasta_tc = '${tc}' and not b_no is null;`);
+
+    toReturn = [];
+
+    str = `${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`;
+    b_nos = str.split("\n");
+
+    for (let i = 0; i < b_nos.length; i++) {
+        var entry = await client.query(`SELECT * FROM Rapor WHERE b_no = '${b_nos[i]}';`);
+        var reportStr = `${entry.rows.map((r) => Object.values(r).join('\t')).join('\n')}`;
+        report = reportStr.split("\n");
+        arr = report[0].split("\t");
+        if (arr[0] != '')
+            toReturn.push(arr);
+    }
+
+    await client.end();
+    return toReturn;
+}
+
+async function getPrescriptionsOfPatient(tc) {
+    const client = await getClient();
+    const entries = await client.query(`SELECT b_no FROM Randevu WHERE hasta_tc = '${tc}' and not b_no is null;`);
+
+    toReturn = [];
+
+    str = `${entries.rows.map((r) => Object.values(r).join('\t')).join('\n')}`;
+    b_nos = str.split("\n");
+
+    for (let i = 0; i < b_nos.length; i++) {
+        var entry = await client.query(`SELECT * FROM Recete WHERE b_no = '${b_nos[i]}';`);
+        var receteStr = `${entry.rows.map((r) => Object.values(r).join('\t')).join('\n')}`;
+        recete = receteStr.split("\n");
+        arr = recete[0].split("\t");
+        if(arr[0] != '')
+            toReturn.push(arr);
+    }
+
+    await client.end();
+    return toReturn;
+}
+
+(async () => {
+    console.log(await getPrescriptionsOfPatient("12312312344"));
+})()
--- index.js
+++ index.js
@@ -23,3 +23,20 @@ app.get('/', function(res,req){
 app.listen(3000,function(){
     console.log("port 3000");
 });
+
+//DATABASE CONNECTION
+const { Client } = require('pg');
+require('dotenv').config();
+
+module.exports.getClient = async () => {
+    const client = new Client({
+        host: "localhost",
+        port: "5432",
+        user: "postgres",
+        password: "mklp%123",
+        database: "372",
+        ssl: false,
+    });
+    await client.connect();
+    return client;
+};
